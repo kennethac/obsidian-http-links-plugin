97583e8b591d8f396457e16af2f394de467d00b7
Kenneth Christensen <1kennethac@gmail.com>
2025-07-11 07:40:56 -0600

    Initial version of plugin

--- main.ts
+++ main.ts
@@ -1,89 +1,95 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface HttpLinkMakerSettings {
+	httpLinkBase: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: HttpLinkMakerSettings = {
+	httpLinkBase: "https://obsidian-http-links.1kennethac.workers.dev",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class HttpLinkMakerPlugin extends Plugin {
+	settings: HttpLinkMakerSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// copy the obsidian URL for the current file to the clipboard
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "copy-obsidian-url",
+			name: "Copy Obsidian URL",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				// Get the current file
+				const file = this.app.workspace.getActiveFile();
+				if (file) {
+					// Get the vault name
+					const vaultName = this.app.vault.getName();
+					// Create the URL
+					const url = `${
+						this.settings.httpLinkBase
+					}/v/${encodeURIComponent(vaultName)}/r/${encodeURI(
+						file.path
+					)}`;
+					// Copy the URL to the clipboard
+					navigator.clipboard.writeText(url);
+					// Optionally, show a success message (e.g., using `app.displayNotice`)
+					new Notice(`Copied URL for ${file.name}!`);
+				} else {
+					new Notice("No active file to copy URL from.");
 				}
-			}
+			},
+			hotkeys: [], // Add your desired hotkeys here if needed
 		});
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, editor, view, leaf) => {
+				console.log("It ran!!");
+				menu.addItem((item) => {
+					item.setTitle("Copy HTTP Obsidian URL")
+						.setIcon("document")
+						.onClick(async () => {
+							// Commands does indeed exist.
+							(this.app as any).commands.executeCommandById(
+								"http-link-maker:copy-obsidian-url"
+							);
+						});
+				});
+			})
+		);
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor, view) => {
+				console.log("It ran!!");
+				menu.addItem((item) => {
+					item.setTitle("Copy HTTP Obsidian URL")
+						.setIcon("document")
+						.onClick(async () => {
+							// Commands does indeed exist.
+							(this.app as any).commands.executeCommandById(
+								"http-link-maker:copy-obsidian-url"
+							);
+							new Notice("Copied to clipboard!");
+						});
+				});
+			})
+		);
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,44 +97,31 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: HttpLinkMakerPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HttpLinkMakerPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Obsidian Link Base")
+			.setDesc("The base URL for your Obsidian links")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your Obsidian link base URL")
+					.setValue(this.plugin.settings.httpLinkBase)
+					.onChange(async (value) => {
+						this.plugin.settings.httpLinkBase = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
